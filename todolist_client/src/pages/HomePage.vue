c04709cc969b84f80fa1b555231d855e5f31452c
Simon Franken <simon.franken@aracom.de>
2022-11-10 16:08:28 +0100

    reset HomePage.vue

--- todolist_client/src/pages/HomePage.vue
+++ todolist_client/src/pages/HomePage.vue
@@ -1,22 +1,2 @@
-<script lang="ts" setup>
-import TodoEntryComponent from "@/components/TodoEntryComponent.vue";
-import { TodoEntry } from "@/models";
-import { ref } from "vue";
-const todoEntry = ref<TodoEntry>({
-  Name: "Lorem ipsum",
-  Desciption:
-    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et",
-  Done: false,
-});
-
-const handleEntryChanged = (todoEntry: TodoEntry) => {
-  console.log(todoEntry);
-};
-</script>
-<template>
-  <TodoEntryComponent
-    :todo-entry="todoEntry"
-    @entry-changed="handleEntryChanged"
-  >
-  </TodoEntryComponent>
-</template>
+<script lang="ts" setup></script>
+<template>HOME</template>
