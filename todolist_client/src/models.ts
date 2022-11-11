f48de0943998dc1f6ca5b4bccda8ab6f66db9c21
Simon Franken <simon.franken@aracom.de>
2022-11-11 12:06:34 +0100

    added todoentry model

--- todolist_client/src/models.ts
+++ todolist_client/src/models.ts
@@ -1,9 +1,14 @@
-import type { RouteRecordRaw } from "vue-router";
-
 export interface route {
   path: string;
   component: any;
   key: string;
   icon: string;
   displayName: string;
+}
+
+export interface TodoEntry {
+  entryId?: string;
+  name: string;
+  description: string;
+  done: boolean;
 }
