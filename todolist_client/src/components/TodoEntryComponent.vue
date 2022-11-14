6a8c6c8802a9577e9c107c5cc528f1b7a1344691
Simon Franken <simon.franken@aracom.de>
2022-11-14 11:25:00 +0100

    added Edit, Delete dropdown + implemented add

--- todolist_client/src/components/TodoEntryComponent.vue
+++ todolist_client/src/components/TodoEntryComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { TodoEntry } from "@/models";
-import { reactive, ref, watch } from "vue";
+import { reactive, ref } from "vue";
 
 const props = defineProps({
   todoEntry: {
@@ -15,33 +15,65 @@ const props = defineProps({
 
 const events = defineEmits<{
   (e: "onChange", todoEntry: TodoEntry): void;
+  (e: "onDelete", todoEntry: TodoEntry): void;
 }>();
 
 const todoEntryState = reactive<TodoEntry>(props.todoEntry);
 
 const editMode = ref<boolean>(props.editModeDefault);
 
-const onDone = () => {
+const handleDone = () => {
   todoEntryState.done = todoEntryState.done ? false : true;
+  events("onChange", todoEntryState);
 };
 
-watch(todoEntryState, () => {
+const handleSave = () => {
   events("onChange", todoEntryState);
-});
+  editMode.value = false;
+};
+
+const handleDelete = () => {
+  events("onDelete", todoEntryState);
+};
 </script>
 <template>
-  <div class="container">
+  <div class="mb-3">
     <div class="" v-if="!editMode">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">
-            <div v-if="todoEntryState.done">
-              <del>{{ todoEntryState.name }}</del>
+          <div class="card-title row">
+            <h5 class="col">
+              <div v-if="todoEntryState.done">
+                <del>{{ todoEntryState.name }}</del>
+              </div>
+              <div v-else>
+                {{ todoEntryState.name }}
+              </div>
+            </h5>
+            <div class="col-md-auto dropdown-center">
+              <button
+                class="btn btn-sm dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              />
+              <ul class="dropdown-menu">
+                <li>
+                  <button
+                    class="dropdown-item"
+                    @click="() => (editMode = true)"
+                  >
+                    Edit
+                  </button>
+                </li>
+                <li>
+                  <button class="dropdown-item" @click="handleDelete">
+                    Delete
+                  </button>
+                </li>
+              </ul>
             </div>
-            <div v-else>
-              {{ todoEntryState.name }}
-            </div>
-          </h5>
+          </div>
           <p class="card-text"></p>
           <div v-if="todoEntryState.done">
             <del>
@@ -51,19 +83,17 @@ watch(todoEntryState, () => {
           <div v-else>
             {{ todoEntryState.description }}
           </div>
-          <div />
-        </div>
-        <div class="card-footer d-flex justify-content-between">
-          <button class="btn btn-warning" @click="() => (editMode = true)">
-            Edit
-          </button>
-          <button
-            class="btn"
-            :class="todoEntryState.done ? 'btn-outline-danger' : 'btn-success'"
-            @click="onDone"
-          >
-            {{ todoEntryState.done ? "Undo" : "Done" }}
-          </button>
+          <div class="d-flex justify-content-end">
+            <button
+              class="btn"
+              :class="
+                todoEntryState.done ? 'btn-outline-danger' : 'btn-success'
+              "
+              @click="handleDone"
+            >
+              {{ todoEntryState.done ? "Undo" : "Done" }}
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -76,26 +106,27 @@ watch(todoEntryState, () => {
               class="form-control"
               v-model="todoEntryState.name"
               v-mode.lazy="todoEntryState.name"
+              placeholder="Titel"
             />
           </h5>
-          <div class="card-text">
+          <div class="card-text mb-2">
             <textarea
               v-model="todoEntryState.description"
               v-mode.lazy="todoEntryState.description"
               class="form-control"
               style="resize: none"
               rows="4"
+              placeholder="Beschreibung"
             />
           </div>
-          <div />
-        </div>
-        <div class="card-footer d-flex justify-content-end">
-          <input
-            type="button"
-            class="btn btn-success"
-            value="Save"
-            @click="() => (editMode = false)"
-          />
+          <div class="d-flex justify-content-end">
+            <input
+              type="button"
+              class="btn btn-success"
+              value="Save"
+              @click="handleSave"
+            />
+          </div>
         </div>
       </div>
     </div>
