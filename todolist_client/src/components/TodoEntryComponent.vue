29ba7d1e0cb422ef2cb9ab0c3facda8065397bd8
Simon Franken <simon.franken@aracom.de>
2022-11-16 11:56:31 +0100

    enter edit mode by clicking on the input field

--- todolist_client/src/components/TodoEntryComponent.vue
+++ todolist_client/src/components/TodoEntryComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { TodoEntry } from "@/models";
-import { reactive, ref } from "vue";
+import { reactive, ref, watch } from "vue";
 
 const props = defineProps({
   todoEntry: {
@@ -13,119 +13,165 @@ const props = defineProps({
   },
 });
 
+const state = reactive<{
+  todoEntry: TodoEntry;
+  editMode: {
+    title: boolean;
+    description: boolean;
+  };
+}>({
+  todoEntry: props.todoEntry,
+  editMode: {
+    title: props.editModeDefault,
+    description: props.editModeDefault,
+  },
+});
+
+const inputTitle = ref<any>(null);
+const inputDescription = ref<any>(null);
+
 const events = defineEmits<{
-  (e: "onChange", todoEntry: TodoEntry): void;
+  (e: "onSave", todoEntry: TodoEntry): void;
   (e: "onDelete", todoEntry: TodoEntry): void;
 }>();
 
-const todoEntryState = reactive<TodoEntry>(props.todoEntry);
-
-const editMode = ref<boolean>(props.editModeDefault);
-
 const handleDone = () => {
-  todoEntryState.done = todoEntryState.done ? false : true;
-  events("onChange", todoEntryState);
+  state.todoEntry.done = state.todoEntry.done ? false : true;
+  events("onSave", state.todoEntry);
 };
 
 const handleSave = () => {
-  events("onChange", todoEntryState);
-  editMode.value = false;
+  events("onSave", state.todoEntry);
 };
 
 const handleDelete = () => {
-  events("onDelete", todoEntryState);
+  events("onDelete", state.todoEntry);
 };
+
+const handleEditMode = (e: any) => {
+  switch (e.target.id) {
+    case "title":
+      state.editMode.title = true;
+      break;
+    case "description":
+      state.editMode.description = true;
+      break;
+    default:
+      state.editMode = { title: true, description: true };
+      break;
+  }
+};
+
+watch(inputTitle, () => {
+  if (state.editMode.title) {
+    inputTitle.value.focus();
+  }
+});
+
+watch(inputDescription, () => {
+  if (state.editMode.description && !state.editMode.title) {
+    inputDescription.value.focus();
+  }
+});
 </script>
 <template>
   <div class="mb-3">
-    <div class="" v-if="!editMode">
+    <div>
       <div class="card">
         <div class="card-body">
-          <div class="card-title row">
-            <h5 class="col">
-              <div v-if="todoEntryState.done">
-                <del>{{ todoEntryState.name }}</del>
+          <div>
+            <div v-if="!state.editMode.title" class="card-title row">
+              <h5 class="col">
+                <div
+                  id="title"
+                  :class="
+                    state.todoEntry.done ? 'text-decoration-line-through' : ''
+                  "
+                  @click="handleEditMode"
+                >
+                  {{ state.todoEntry.name }}
+                </div>
+              </h5>
+              <div class="col-md-auto dropdown-center">
+                <button
+                  class="btn btn-sm dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                />
+                <ul class="dropdown-menu">
+                  <li>
+                    <button class="dropdown-item" @click="handleEditMode">
+                      Edit
+                    </button>
+                  </li>
+                  <li>
+                    <button class="dropdown-item" @click="handleDelete">
+                      Delete
+                    </button>
+                  </li>
+                </ul>
               </div>
-              <div v-else>
-                {{ todoEntryState.name }}
-              </div>
-            </h5>
-            <div class="col-md-auto dropdown-center">
-              <button
-                class="btn btn-sm dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              />
-              <ul class="dropdown-menu">
-                <li>
-                  <button
-                    class="dropdown-item"
-                    @click="() => (editMode = true)"
-                  >
-                    Edit
-                  </button>
-                </li>
-                <li>
-                  <button class="dropdown-item" @click="handleDelete">
-                    Delete
-                  </button>
-                </li>
-              </ul>
+            </div>
+            <div v-else>
+              <h5 class="card-title">
+                <input
+                  ref="inputTitle"
+                  type="text"
+                  class="form-control"
+                  v-model="state.todoEntry.name"
+                  placeholder="Titel"
+                />
+              </h5>
             </div>
           </div>
-          <p class="card-text"></p>
-          <div v-if="todoEntryState.done">
-            <del>
-              {{ todoEntryState.description }}
-            </del>
-          </div>
-          <div v-else>
-            {{ todoEntryState.description }}
+          <div class="mb-3">
+            <div
+              id="description"
+              v-if="!state.editMode.description"
+              :class="
+                state.todoEntry.done ? 'text-decoration-line-through' : ''
+              "
+              @click="handleEditMode"
+            >
+              {{ state.todoEntry.description }}
+            </div>
+            <div v-else>
+              <textarea
+                ref="inputDescription"
+                class="form-control"
+                style="resize: none"
+                placeholder="Beschreibung"
+                v-model="state.todoEntry.description"
+              />
+            </div>
           </div>
           <div class="d-flex justify-content-end">
             <button
+              v-if="!state.editMode.title && !state.editMode.description"
               class="btn"
               :class="
-                todoEntryState.done ? 'btn-outline-danger' : 'btn-success'
+                state.todoEntry.done ? 'btn-outline-danger' : 'btn-success'
               "
               @click="handleDone"
             >
-              {{ todoEntryState.done ? "Undo" : "Done" }}
+              {{ state.todoEntry.done ? "Undo" : "Done" }}
             </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div v-if="editMode">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">
-            <input
-              type="text"
-              class="form-control"
-              v-model="todoEntryState.name"
-              v-mode.lazy="todoEntryState.name"
-              placeholder="Titel"
-            />
-          </h5>
-          <div class="card-text mb-2">
-            <textarea
-              v-model="todoEntryState.description"
-              v-mode.lazy="todoEntryState.description"
-              class="form-control"
-              style="resize: none"
-              rows="4"
-              placeholder="Beschreibung"
-            />
-          </div>
-          <div class="d-flex justify-content-end">
-            <input
-              type="button"
+            <button
+              v-else
               class="btn btn-success"
-              value="Save"
-              @click="handleSave"
-            />
+              @click="
+                () => {
+                  state.editMode = {
+                    title: false,
+                    description: false,
+                  };
+                  handleSave();
+                }
+              "
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>
